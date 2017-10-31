124b6fb9f7ab3d4d364e079cc6a85990ab23b7aa
Evan Wappel <ewappel@gmail.com>
2017-10-31 09:38:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 # javascript_notes
 
+## data types
+console.log('JavaScript')
+console.log(33.7)
+console.log(true)
+console.log(null)
+
+## 
 // Log Codecademy in all uppercase letters
 console.log('Codecademy'.toUpperCase()); 
 
