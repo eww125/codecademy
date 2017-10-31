4045e0ba204fb5b56d6a0db283118173b625d69c
Evan Wappel <ewappel@gmail.com>
2017-10-31 16:58:28 -0700

    Update control_flow.js

--- control_flow.js
+++ control_flow.js
@@ -42,3 +42,11 @@ if (!favoritePhrase) {
 } else {
   console.log('This string is definitely empty.');
 }
+
+// comparison operator
+let hungerLevel = 10
+if (hungerLevel > 7) {
+    console.log('Time to eat!');
+} else {
+    console.log('We can eat later!');
+}
