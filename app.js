64f686816750bec6ab3c79f046bb77244b16dbc7
Evan Wappel <ewappel@gmail.com>
2017-10-31 12:31:14 -0700

    Update app.js

--- app.js
+++ app.js
@@ -81,3 +81,18 @@ console.log(notDefined);
 
 let valueless
 console.log(valueless);
+
+
+// Mathematical Assignment Operators
+let molecule = 16;
+let particle = 18;
+let assay = 3;
+
+// Add and assign to molecule below
+molecule += 16
+
+// Multiply and assign to particle below
+particle *=6.02
+
+// Increment assay by 1
+assay++
