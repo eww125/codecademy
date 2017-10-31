a073deee788f913da8b5e3223c626ba0b8d07dac
Evan Wappel <ewappel@gmail.com>
2017-10-31 09:40:32 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-# javascript_notes
-
-## data types
+// data types
 console.log('JavaScript')
 console.log(33.7)
 console.log(true)
 console.log(null)
 
-## 
+// math operators
+console.log(45 + 3.5)
+console.log(2017-1969)
+console.log(65/240)
+console.log(0.2708*100)
+
+//properties
+console.log('Teaching the world how to code'.length);
+
 // Log Codecademy in all uppercase letters
 console.log('Codecademy'.toUpperCase()); 
 
 // Use a string method to log the following statment without whitespace at the beginning and end of it.
 console.log('    Remove whitespace   '.trim()); 
 
-string.proto methods
+// string.proto methods
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/prototype
