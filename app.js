a2131e11c335a3643d256ade2b9544a407665de1
Evan Wappel <ewappel@gmail.com>
2017-10-31 11:21:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -27,3 +27,6 @@ console.log(Math.random()); // random number between 0 and 1
 
 // Math
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8))
