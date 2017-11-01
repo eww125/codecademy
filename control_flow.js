99c88cf9f701ea8a6c9f10d90ee00b26a36dce56
Evan Wappel <ewappel@gmail.com>
2017-11-01 13:22:11 -0700

    Update control_flow.js

--- control_flow.js
+++ control_flow.js
@@ -83,3 +83,21 @@ if (moonPhase === 'full' || isFoggyNight === true) {
 } else {
   console.log('Invalid moon phase');
 }
+
+// switch statements
+let moonPhase = 'full';
+
+switch (moonPhase) {
+  case 'full':
+    console.log('Howl');
+    break;
+  case 'mostly full':
+    console.log('Arms and legs are getting hairier');
+    break;
+  case 'mostly new':
+    console.log('Back on two feet');
+    break;
+  default:
+    console.log('Invalid moon phase');
+    break;
+}
