f001468cf56e6c6246394f73bfb7a3e0928cb53b
Evan Wappel <ewappel@gmail.com>
2017-11-01 13:31:48 -0700

    Update control_flow.js

--- control_flow.js
+++ control_flow.js
@@ -101,3 +101,21 @@ switch (moonPhase) {
     console.log('Invalid moon phase');
     break;
 }
+
+// ternary operator
+let isLocked = false;
+isLocked ?
+  console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+
+let isCorrect = true;
+isCorrect ?
+  console.log('Correct!') :
+  console.log('Incorrect!');
+
+
+let favoritePhrase = 'Love That!';
+favoritePhrase === 'Love That!' ?
+  console.log('I love that!')
+:
+  console.log("I don't love that!");
