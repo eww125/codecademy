8cbe517fe645827c14fac64e6c965468d31a2c08
Evan Wappel <ewappel@gmail.com>
2017-11-01 17:24:31 -0700

    Create race_day.js

---
+++ race_day.js
@@ -0,0 +1,9 @@
+let registeredEarly = Math.round(Math.random())
+console.log(registeredEarly)
+
+const runnersAge = 45
+
+let raceNumber = Math.floor(Math.random()*1000);
+console.log(raceNumber)
+
+registeredEarly == '1' ? console.log('early') : console.log('same day');
