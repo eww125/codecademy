1107ac9fa5eadd6ff7e2fa5b0d73d1a830afa771
Evan Wappel <ewappel@gmail.com>
2017-11-03 16:00:13 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -19,3 +19,8 @@ const isGreaterThan = (numberOne, numberTwo) => {
 }
 
 console.log(isGreaterThan(30,20))
+
+
+// arrow functions
+const volumeOfSphere = diameter => (1/6) * Math.PI * diameter * diameter * diameter;
+console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
