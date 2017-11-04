95e4fb16ac5074b455fcc60f19e811f0421cf8c1
Evan Wappel <eww125@evans-mbp.attlocal.net>
2017-11-04 09:27:40 -0700

    rock

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,42 +1,50 @@
 userChoice = 'rock'
-console.log(`user_choice = ${userChoice}`)
+console.log(`userChoice = ${userChoice}`)
 
 const getUserChoice = (userInput) => {
   userInput = userInput.toLowerCase();
-  
   if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
     return userInput
 } else {
-  console.log('Invalid input');
+  console.log('Invalid input'); }
 }
-}
 
-
-
-const getComputerChoice = () => {
-	let randomNumber = Math.floor(Math.random() * 3)
-  console.log(`random number = ${randomNumber}`)
-  if (randomNumber === 0) {
-    return console.log('computer choice: rock')
+function getComputerChoice() {
+  // generate and print random number
+  let randomNumber = Math.floor(Math.random() * 3)
+  console.log(`randomNumber = ${randomNumber}`)
+  // convert 0/1/2 into rock/paper/scissors
+  if (randomNumber===0) {
+    return 'rock'
+} else if (randomNumber===1) {
+    return 'paper'
+} else {
+    return 'scissors'
   }
-	console.log(`Computer Choice = ${randomNumber}`)
 }
 
-getComputerChoice()
+computerChoice = getComputerChoice()
 
-
-/*
-const determineWinner = (userChoice, computerChoice) => {
-  if (userChoice = computerChoice) {
-    return console.log('tie game');
+function determineWinner(userChoice, computerChoice) {
+  console.log(userChoice, computerChoice)
+  if (userChoice === computerChoice) {
+    return 'The game was a tie'
   }
-  
-  if (UserChoice = 'rock') {
-    if (computerChoice = 'scissors') {
-      return console.log('rock crushes scissors')
+  if (userChoice === 'rock') {
+    if (computerChoice === 'scissors') {
+      return 'The user won'
+    }
+    else {
+      return 'The computer won'
+    }
     }
   }
+
+  //if (UserChoice = 'rock') {
+  //  if (computerChoice = 'scissors') {
+    //  return console.log('rock crushes scissors')
+    //}
+  //}
 }
 
 determineWinner(userChoice, computerChoice)
-*/
